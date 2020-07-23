3738b351b523bf32dc28e8d5345a7320136b2daa
JohnDoe-309 <55737789+JohnDoe-309@users.noreply.github.com>
2020-07-23 08:58:47 +0530

    Add files via upload
    Yet to add exception handling and depth perception. Code Works!

--- minimax.py
+++ minimax.py
@@ -1,37 +1,41 @@
 import copy
-def create():
+def create(): # this is to create a new emptty board
     ttt = [["-","-","-"],
         ["-","-","-"],
         ["-","-","-"]]
     return ttt
 
-scores = {'X':1,'O':-1,'tie':0}
+scores = {'X':-1,'O':1,'tie':0}
 
-def dboard(ttt):
+def dboard(ttt): # this function displays the current state of the TTT board
     print(ttt[0][0]+"|"+ttt[0][1]+"|"+ttt[0][2])
     print(ttt[1][0]+"|"+ttt[1][1]+"|"+ttt[1][2])
     print(ttt[2][0]+"|"+ttt[2][1]+"|"+ttt[2][2])
-    return
+    
 
-def p1play(ttt):
+def p1play(ttt): # this is the player 1s turn, he chooses from a set of indices available on the board
     l = list()
     for i in range(3):
         for j in range(3):
             if (ttt[i][j]=="-"):
                 l.append([i,j])
     
+    if not l:
+        print("Tie")
+        exit(1)
+
     print(l)
     x = int(input("Choose the index of the location from the above options : "))
     ttt[l[x][0]][l[x][1]] = "X"
 
-def wonyet(ttt):
+def wonyet(ttt): # this is to check the winner of the board and allot scores to the winner
     for i in range(2):
         j = 0
         if (ttt[i][j]==ttt[i][j+1] and ttt[i][j+1]==ttt[i][j+2]):
             if (ttt[i][j]=="X"): 
-                return "X"
+                return "X" # if X wins
             if (ttt[i][j]=="O"):
-                return "O"
+                return "O" # if O wins
 
         elif (ttt[j][i]==ttt[j+1][i] and ttt[j+1][i]==ttt[j+2][i]):
             if (ttt[j][i]=="X"): 
@@ -47,11 +51,12 @@ def wonyet(ttt):
     
     for i in range(3):
         for j in range(3):
-            return "continue" 
+            if (ttt[i][j]=="-"):
+                return "continue" # if game not over yet
     
-    return "tie" #tie
+    return "tie" # tie
 
-def wonfn(ttt):
+def wonfn(ttt): # this is the final win fn, checks the board to see if any player has won/tied the game
     for i in range(2):
         j = 0
         if (ttt[i][j]==ttt[i][j+1] and ttt[i][j+1]==ttt[i][j+2]):
@@ -68,12 +73,22 @@ def wonfn(ttt):
         if (ttt[i][i]=="X" or ttt[i][i]=="O"):
             print(ttt[i][i]+" Wins")
             exit(1)
+    
+    count = 0
+    for i in range(3):
+        for j in range(3):
+            if (ttt[i][j]=="-"):
+                continue
+            else:
+                count+=1
+    if (count==9): print("Tie") # prints tie if all the squares of the board are filled
 
-def bestmove(ttt):
+
+def bestmove(ttt): # to check the best possible move for the AI
     #Ais turn
-    bestscore = -20
-    x = -1
-    y = -1
+    bestscore = -2 # any number less than -1
+    x = -1 # initialising x to an out of bounds value
+    y = -1 # initialising Y to an out of bounds value
     for i in range(3):
         for j in range(3):
             if (ttt[i][j]=='-'):
@@ -91,29 +106,29 @@ def minimax(ttt, aiturn):
         return scores[wonyet(ttt)]
 
     if (aiturn):
-        bestscore = -20
+        bestscore = -2 # any number less than -1
         for i in range(3):
             for j in range(3):
                 if (ttt[i][j]=='-'):
                     ttt[i][j] = "O"
-                    score = minimax(ttt, False)
+                    score = minimax(ttt, False) # shifts turn to p1
                     ttt[i][j] = "-"
-                    bestscore = max(score, bestscore)
+                    bestscore = max(score, bestscore) # maximising the value of the ai player
         return bestscore
     else :
-        bestscore = -20
+        bestscore = 2 # any number greater than 1
         for i in range(3):
             for j in range(3):
                 if (ttt[i][j]=='-'):
                     ttt[i][j] = "X"
-                    score = minimax(ttt, True)
+                    score = minimax(ttt, True) # shifts turn to ai
                     ttt[i][j] = "-"
-                    bestscore = min(score, bestscore)
+                    bestscore = min(score, bestscore) # minimising the value of the ai player
         return bestscore
 
-won = 0
-ttt = create()
-while (won!=1 or won!=-1 or won!="tie"):
+won = 0 
+ttt = create() # creating board
+while (won==0): 
     p1play(ttt)
     dboard(ttt)
     wonfn(ttt)
